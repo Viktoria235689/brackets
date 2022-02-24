d44cca930492effe13a40e0ef7a98d372739ed69
viktoria235689 <400158@tut.by>
2022-02-24 22:54:57 +0300

    feat: the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let i = 0;
+
+  while (i < str.length) {
+    let isInclude = false;
+    bracketsConfig.forEach((element) => {
+      if (str.includes(element[0] + element[1])) {
+        str = str.replace(element[0] + element[1], "");
+        isInclude = true;
+      }
+    });
+    if (str.length === 0) {
+      return true;
+    }
+    if (!isInclude) {
+      return false;
+    }
+  }
+};
